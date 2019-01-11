8b7f9cdad16c5db6a0cd831fca83344a9d0d274e
pikopiri127 <hb.ucfbootcamp@gmail.com>
2019-01-10 21:32:01 -0500

    commit

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+// Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+
+// Create an instance of the express app.
+var app = express();
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -12,10 +20,15 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
 
-// Initialize Express
-var app = express();
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI.{ useNewUrlParser: true });
+
 
 // Configure middleware
 
@@ -27,121 +40,121 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 // ---------
 // Routes
 // ---------
-
+// Handlebar
+// Handlebars requires an object to be sent to the index handlebars file.
+app.get("/", function (req, res) { 
+    res.render("index");        // this view should be under ...scraping/views
+});
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.developer-tech.com/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+    // First, we grab the body of the html with axios
+    axios.get("https://www.developer-tech.com/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h3").each(function (i, element) {
-      // Save an empty result object
-      var result = {};
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article h3").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
 
 
-      // Create a new Article using the `result` object built from scraping
-      // ----------------------------//
-      // insert if not exists mongodb//
-      // ----------------------------//
-      // https://gist.github.com/ykst/d1a024f1d800311744be
-      // https://docs.mongodb.com/manual/reference/operator/update/setOnInsert/#op._S_setOnInsert
-      // https://stackoverflow.com/questions/16358857/mongodb-atomic-findorcreate-findone-insert-if-nonexistent-but-do-not-update
-      // db.Article.findAndModify({query: {i:0}, update: {$setOnInsert: {i: 0}}, upsert: true})
+            // Create a new Article using the `result` object built from scraping
+            // ----------------------------//
+            // insert if not exists mongodb//
+            // ----------------------------//
+            // https://gist.github.com/ykst/d1a024f1d800311744be
+            // https://docs.mongodb.com/manual/reference/operator/update/setOnInsert/#op._S_setOnInsert
+            // https://stackoverflow.com/questions/16358857/mongodb-atomic-findorcreate-findone-insert-if-nonexistent-but-do-not-update
+            // db.Article.findAndModify({query: {i:0}, update: {$setOnInsert: {i: 0}}, upsert: true})
 
-      db.Article.update(result, true, false)
-        //      db.Article.create(result)
-        .then(function (dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function (err) {
-          // If an error occurred, log it
-          console.log(err);
+            db.Article.update(result, true, false)
+                //      db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
         });
-    });
 
-    // Send a message to the client
-    res.send("Scrape Complete");
-  });
+        // Send a message to the client
+        res.send("Scrape Complete");
+    });
 });
 
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function (dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function (dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function (dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
-// Start the server
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-  console.log("App running on port " + PORT + "!");
+    // Log (server-side) when our server has started
+    console.log("App running on: http://localhost:" + PORT);
 });
