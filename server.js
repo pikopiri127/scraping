e360d9e58bd5291396338e646c755f509e644365
pikopiri127 <hb.ucfbootcamp@gmail.com>
2019-01-10 21:34:38 -0500

    commit2

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var PORT = process.env.PORT || 3000;
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI.{ useNewUrlParser: true });
+mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
 
 // Configure middleware
